aa082cf002095ade89f81fd5d7137d97220467d5
llsanketll <sanket.lamsal@gmail.com>
2022-05-07 21:41:11 +0545

    Final commit of the day of sem

--- app/src/Components/HotelCard.js
+++ app/src/Components/HotelCard.js
@@ -1,21 +1,25 @@
-import HotelCardContainer from "../Styles/HotelCardContainer";
-import Button from "./Button";
+import HotelCardContainer from '../Styles/HotelCardContainer';
+import Button from './Button';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
 import StarIcon from '@mui/icons-material/Star';
-function HotelCard(props) {
 
+function HotelCard(props) {
   return (
     <HotelCardContainer>
       <div className="HotelCard-ImageContainer">
-        <img src="https://images.unsplash.com/photo-1517840901100-8179e982acb7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" />
+        <img src={props.main_photo_url} />
       </div>
       <div className="HotelCard-Middle">
-        <h2 className="HotelCard-name">HotelName</h2>
+        <h2 className="HotelCard-name">{props.hotel_name}</h2>
         <div className="HotelCard-LocationContainer">
           <h4>Location</h4>
-          <p className="Location">Naxal</p>
+          <p className="Location">{props.address}</p>
         </div>
-        <Button variant="outlined" color="#0071C2" className="HotelCard-ViewMap">
+        <Button
+          variant="outlined"
+          color="#0071C2"
+          className="HotelCard-ViewMap"
+        >
           <LocationOnIcon />
           View Map
         </Button>
@@ -24,7 +28,7 @@ function HotelCard(props) {
           <div>
             <div className="HotelCard-Rating">
               <StarIcon />
-              4.5
+              {props.review_score}
             </div>
             <Button variant="outlined" color="#0071C2">
               View Review
@@ -33,12 +37,16 @@ function HotelCard(props) {
         </div>
       </div>
       <div className="HotelCard-Last">
-        <h3 className="HotelCard-Price"> Rs.1500</h3>
+        <h3 className="HotelCard-Price">
+          $ {props.composite_price_breakdown.gross_amount_per_night.value}
+        </h3>
         <div>* per night</div>
-        <Button color="#0071C2" fontColor="white">Go To Website</Button>
+        <Button color="#0071C2" fontColor="white">
+          Go To Website
+        </Button>
       </div>
     </HotelCardContainer>
-  )
+  );
 }
 
 export default HotelCard;
